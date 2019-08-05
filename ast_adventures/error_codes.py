352bbae61cba74be9af1c88d6f45e70c98b4b7fe
sco1 <sco1.git@gmail.com>
2019-08-05 12:50:17 -0400

    Frame proposed error codes as simple strings

---
+++ ast_adventures/error_codes.py
@@ -0,0 +1,19 @@
+# Scratchpad for error codes
+# TODO: Replace with a custom Error object that has the message, line number, column, etc.
+
+# Function Annotations
+TYP001 = "Missing type annotation for function argument"
+TYP002 = "Missing type annotation for *args"
+TYP003 = "Missing type annotation for **kwargs"
+
+# Method Annotations
+TYP101 = "Missing type annotation for self in class method"
+TYP102 = "Missing type annotation for cls in classmethod"
+
+# Return Annotations
+TYP201 = "Missing return type annotation for public function"
+TYP202 = "Missing return type annotation for protected function"
+TYP203 = "Missing return type annotation for secret function"
+TYP204 = "Missing return type annotation for magic function"
+TYP205 = "Missing return type annotation for staticmethod"
+TYP206 = "Missing return type annotation for classmethod"
