5ea3267c418125691ecfbfb1b6048d37e9438d6a
sco1 <sco1.git@gmail.com>
2019-08-05 14:00:49 -0400

    Add dependency pinning

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import find_packages, setup
 import sys
+
+from setuptools import find_packages, setup
 
 assert sys.version_info >= (3, 6, 0), "ast-adventures requires Python 3.6+"
 
@@ -32,17 +33,16 @@ setup(
         "Topic :: Software Development :: Quality Assurance",
     ],
     install_requires=[
-        "flake8>=3.7,<3.8"
+        "flake8~=3.7.8"
     ],
     extras_require={
         "dev": [
-            "flake8-bugbear",
-            "flake8-docstrings"
-            "flake8-import-order",
-            "flake8-string-format",
-            "flake8-tidy-imports",
-            "flake8-todo",
-            "pre-commit",
+            "flake8-bugbear~=19.3",
+            "flake8-docstrings~=1.3",
+            "flake8-import-order~=0.18",
+            "flake8-string-format~=0.2",
+            "flake8-tidy-imports~=2.0",
+            "flake8-todo~=0.7",
         ]
     }
 )
