fbb7eb37b0bf9b214798ea254e21d497073d9f69
S. Co1 <sco1.git@gmail.com>
2020-01-02 17:18:59 -0500

    Add configuration option for conditional TYP200 suppression
    Adds `--suppress-none-returning` to support suppression of TYP200-level errors for functions that contain no `return` statement or contain only bare `return` statements.

--- flake8_annotations/checker.py
+++ flake8_annotations/checker.py
@@ -1,6 +1,8 @@
+from argparse import Namespace
 from functools import lru_cache
 from typing import Generator, List
 
+from flake8.options.manager import OptionManager
 from flake8_annotations import (
     Argument,
     Function,
@@ -71,6 +73,25 @@ class TypeHintChecker:
             # Yield explicit errors for arguments that are missing annotations
             for arg in function.get_missed_annotations():
                 yield classify_error(function, arg).to_flake8()
+
+    @classmethod
+    def add_options(cls, parser: OptionManager) -> None:
+        """Add custom configuration option(s) to flake8."""
+        parser.add_option(
+            "--suppress-none-returning",
+            default=False,
+            action="store_true",
+            parse_from_config=True,
+            help=(
+                "Suppress TYP200-level errors for functions that contain no return statement or "
+                "contain only bare return statements. (Default: False)"
+            ),
+        )
+
+    @classmethod
+    def parse_options(cls, options: Namespace) -> None:
+        """Parse the custom configuration options given to flake8."""
+        cls.suppress_none_returning = options.suppress_none_returning
 
     @staticmethod
     def get_typed_tree(src: str) -> ast.Module:
