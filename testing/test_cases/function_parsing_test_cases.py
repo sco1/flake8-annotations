9dffb2d684cc080e3c64d6e3c0df630ec2e6181b
S. Co1 <sco1.git@gmail.com>
2020-02-23 11:56:39 -0500

    Add failing test case for nested class parsing
    See: #67

--- testing/test_cases/function_parsing_test_cases.py
+++ testing/test_cases/function_parsing_test_cases.py
@@ -481,4 +481,21 @@ function_test_cases = {
             nonclass_func(name="baz", function_type=FunctionType.PUBLIC),
         ),
     ),
+    "nested_classes": FunctionTestCase(
+        src=dedent(
+            """\
+            class Foo:
+                class Bar:
+                    def bar_method(self):
+                        pass
+
+                def foo_method(self):
+                    pass
+            """
+        ),
+        func=(
+            class_func(name="bar_init", function_type=FunctionType.PUBLIC),
+            class_func(name="foo_init", function_type=FunctionType.PUBLIC),
+        ),
+    ),
 }
