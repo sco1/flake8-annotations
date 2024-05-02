2ce2fa5b64d258e8ccbf3c4b26e8ab047713d504
S. Co1 <sco1.git@gmail.com>
2024-05-02 15:51:04 -0400

    Add `--respect-type-ignore` flag

--- testing/helpers.py
+++ testing/helpers.py
@@ -29,6 +29,7 @@ def check_source(
     allow_untyped_nested: bool = False,
     mypy_init_return: bool = False,
     allow_star_arg_any: bool = False,
+    respect_type_ignore: bool = False,
     dispatch_decorators: t.AbstractSet[str] = frozenset(_DEFAULT_DISPATCH_DECORATORS),
     overload_decorators: t.AbstractSet[str] = frozenset(_DEFAULT_OVERLOAD_DECORATORS),
 ) -> t.Generator[FORMATTED_ERROR, None, None]:
@@ -43,6 +44,7 @@ def check_source(
     checker_instance.allow_untyped_nested = allow_untyped_nested
     checker_instance.mypy_init_return = mypy_init_return
     checker_instance.allow_star_arg_any = allow_star_arg_any
+    checker_instance.respect_type_ignore = respect_type_ignore
     checker_instance.dispatch_decorators = dispatch_decorators
     checker_instance.overload_decorators = overload_decorators
 
